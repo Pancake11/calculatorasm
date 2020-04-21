499c4deec1c7588c9a2e61e64a962df477098f2b
root <root@localhost.localdomain>
2020-04-21 10:51:49 -0400

    bugged working on it

--- calculatrice.asm
+++ calculatrice.asm
@@ -5,7 +5,7 @@ STDIN equ 0
 STDOUT equ 1
 
 section .data
-msgchoix dw 'Bonjour veulliez choisir le type de calcul en rentrant 1:addition 2:mul',0xa
+msgchoix dw 'Bonjour veulliez choisir le type de calcul en rentrant 1:addition 2:mul 3:div 4:sqrt 5:log',0xa
 lenmsgchoix equ $ - msgchoix
 msg1add dw 'Bonjour veuillez choisir un nombre a additionner',0xa
 lenmsg1add equ $ - msg1add
@@ -15,6 +15,12 @@ msg1mul dw 'premier nombre a multiplier',0xa
 lenmsg1mul equ $ - msg1mul
 msg2mul dw 'deuxieme nombre',0xa
 lenmsg2mul equ $ - msg2mul
+msg1div dw "le nombre a diviser",0xa
+lenmsg1div equ $ - msg1div
+msg2div dw "le diviseur",0xa
+lenmsg2div equ $ - msg2div
+msg1sqrt dw "le nombre dont vous voulez la racine",0xa
+lenmsg1sqrt equ  - msg1sqrt
 msg3 dw 'le resultat est : '
 lenmsg3 equ $ - msg3
 msg4 dw '',0xa
@@ -44,10 +50,74 @@ _start:
 	int 0x80
 	
 	mov ecx,[choix]
-	cmp ecx,'1'
+	cmp ecx,0x00000a31
 	je addition
-	jmp multiplication
+	cmp ecx,0x00000a32
+	je multiplication
+	cmp ecx,0x00000a33
+	je division
+	cmp ecx,0x00000a34
+	je sqrt
+
+sqrt :
+	mov eax,SYS_WRITE
+	mov ebx,STDOUT
+	mov ecx,msg1sqrt
+	mov edx,lenmsg1sqrt
+	int 0x80
+
+	mov eax,SYS_READ
+	mov ebx,STDIN
+	mov ecx,num1
+	mov edx,1
+	int 0x80
+
+	;calcul
 	
+	
+	
+	jmp resultat
+
+division:
+	
+	mov eax,SYS_WRITE
+	mov ebx,STDOUT
+	mov ecx,msg1div
+	mov edx,lenmsg1div
+	int 0x80
+
+	mov eax,SYS_READ
+	mov ebx,STDIN
+	mov ecx,num1
+	mov edx,2
+	int 0x80
+
+	mov eax,SYS_WRITE
+	mov ebx,STDOUT
+	mov ecx,msg2div
+	mov edx,lenmsg2div
+	int 0x80
+
+	mov eax,SYS_READ
+	mov ebx,STDIN
+	mov ecx,num2
+	mov edx,2
+	int 0x80
+
+	;calcul
+
+	mov ebx,[num1]
+	sub ebx,'0'
+
+	mov eax,[num2]
+	sub ebx,'0'
+
+	div ebx
+	add ebx,'0'
+	mov [sum],ebx
+
+	jmp resultat
+
 multiplication:
 	
 	mov eax,SYS_WRITE
@@ -129,6 +199,7 @@ addition :
 	jmp resultat
 
 resultat:
+
 	mov eax,SYS_WRITE
 	mov ebx,STDOUT
 	mov ecx,msg3
